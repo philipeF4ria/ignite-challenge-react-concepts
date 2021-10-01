7fc9a297b3ddbd6366266e82e7b6fffbf870a854
Philipe Faria <philipefaria61@gmail.com>
2021-10-01 15:49:20 -0300

    Challenge Completed

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,43 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) {
+      return;
+    }
+
+    const newTask: Task = {
+      id: Math.floor(Math.random() * 1000 + 1),
+      title: newTaskTitle,
+      isComplete: false,
+    };
+
+    console.log(newTask);
+
+    setTasks([...tasks, newTask]);
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+
+    setTasks(prevState => prevState.map(
+      task => (
+        task.id === id 
+          ? {
+            ...task,
+            isComplete: true,
+          }
+          : task
+      ),
+    ));
+
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+
+    const currentTasks = tasks.filter(task => task.id !== id);
+
+    setTasks(currentTasks);
   }
 
   return (
